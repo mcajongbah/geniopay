07ef60826aa6b907751b8d6a596c436d00900886
mcajongbah <malvinajongbah@gmail.com>
2022-03-04 21:29:41 +0000

    setup complete

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+function App() {
+
+  return (
+    <div className="text-3xl text-red-400 font-bold underline">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App
