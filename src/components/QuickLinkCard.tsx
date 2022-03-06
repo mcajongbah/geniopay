b3dd9e562f923642b89c11167976787afc60d3a7
mcajongbah <malvinajongbah@gmail.com>
2022-03-06 09:08:01 +0000

    added a few sections to dashboard

--- src/components/QuickLinkCard.tsx
+++ src/components/QuickLinkCard.tsx
@@ -1,15 +1,21 @@
-
 type QuickLinkCardProps = {
+  link: {
+    name: string;
+    icon: React.ReactNode;
+  };
+};
 
-}
-
-const QuickLinkCard: React.FC<QuickLinkCardProps> = ({}) => {
-        return (
-          <button className="py-6 outline-none px-4 border border-slate-300 rounded-lg flex-col flex">
-            <span className="flex items-center justify-center w-8 h-8 rounded-full"></span>
-            <p className="text-sm text-[#001B21]"></p>
-          </button>
-        );
-}
+const QuickLinkCard: React.FC<QuickLinkCardProps> = ({
+  link: { name, icon },
+}) => {
+  return (
+    <button className="py-6 items-center space-y-4 h-full max-w-[120px] justify-center outline-none px-4 border border-slate-300 rounded-lg flex-col flex">
+      <span className="flex items-center justify-center w-8 h-8 bg-slate-500 rounded-full">
+        {icon}
+      </span>
+      <p className="text-sm text-[#001B21] px-2">{name}</p>
+    </button>
+  );
+};
 
 export default QuickLinkCard;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,9 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { WalletCard } from "../components";
+import { LineChart, QuickLinkCard, WalletCard } from "../components";
+import { WalletIcon } from "../components/Icons";
 
-type DashboardProps = {
-
-}
+type DashboardProps = {};
 
 const wallets = [
   {
@@ -29,32 +29,126 @@ const wallets = [
   },
 ];
 
+const quickLinks = [
+  {
+    name: "Add new Wallet",
+    icon: <WalletIcon />,
+  },
+  {
+    name: "Add new Card",
+    icon: <WalletIcon />,
+  },
+  {
+    name: "Balance Exchange",
+    icon: <WalletIcon />,
+  },
+  {
+    name: "Transfer to Account",
+    icon: <WalletIcon />,
+  },
+  {
+    name: "Generate Voucher",
+    icon: <WalletIcon />,
+  },
+  {
+    name: "Mobile Money",
+    icon: <WalletIcon />,
+  },
+  {
+    name: "Payment Link",
+    icon: <WalletIcon />,
+  },
+];
+
+const transactions = [
+  {
+    name: "Total transactions",
+    value: "$88,600.00",
+    icon: <WalletIcon />,
+  },
+  {
+    name: "Pay-In",
+    value: "$4,600.00",
+    icon: <WalletIcon />,
+  },
+  {
+    name: "Pay-Out",
+    value: "$72,600.00",
+    icon: <WalletIcon />,
+  },
+];
+
 const Dashboard: React.FC<DashboardProps> = ({}) => {
-        return (
-          <div className="p-4 w-full flex flex-col">
-            <div className="w-full items-center flex justify-between">
-              <p className="text-[21px] text-[#001b21]">Wallet (5)</p>
-              <Link to="/wallet" className="text-primary text-base">
-                View all wallets
-              </Link>
-            </div>
+  const [activity, setActivity] = useState("Month");
+  return (
+    <div className="p-4 w-full flex flex-col">
+      <div className="w-full items-center flex justify-between">
+        <p className="text-[21px] text-[#001b21]">Wallet (5)</p>
+        <Link to="/wallet" className="text-primary text-base">
+          View all wallets
+        </Link>
+      </div>
 
-            <div className="grid grid-cols-3 mt-4 gap-x-4">
-              {wallets.map((wallet, index) => (
-                <WalletCard wallet={wallet} key={index} />
-              ))}
-            </div>
+      <div className="grid grid-cols-3 mt-4 gap-x-4">
+        {wallets.map((wallet, index) => (
+          <WalletCard wallet={wallet} key={index} />
+        ))}
+      </div>
 
-            <div className="flex mt-14 flex-col">
-              <p className="text-[#001B21] text-[21px]">Quick Links</p>
-              <p className="text-base text-slate-400">
-                Your frequently used actions for easy access.
-              </p>
-            </div>
+      <div className="flex mt-14 flex-col">
+        <p className="text-[#001B21] text-[21px]">Quick Links</p>
+        <p className="text-base text-slate-400">
+          Your frequently used actions for easy access.
+        </p>
+      </div>
 
-            <div className="flex space-x-4 items-center"></div>
+      <div className="flex space-x-4 mt-6 items-center">
+        {quickLinks.map((link, index) => (
+          <QuickLinkCard key={index} link={link} />
+        ))}
+      </div>
+      <div className="flex flex-col space-y-5">
+        <div className="flex justify-between items-center mt-[72px]">
+          <div className="flex space-x-9 items-center justify-center">
+            <p className="text-[#001b21] text-[21px]">Activity</p>
+
+            <select
+              className="outline-none text-primary text-base"
+              name="activity"
+              id="activity"
+              value={activity}
+              onChange={(e) => setActivity(e.target.value)}
+            >
+              <option value="Week">Week</option>
+              <option value="Month">Month</option>
+              <option value="Year">Year</option>
+            </select>
           </div>
-        );
-}
+          <button className="text-primary text-base">
+            View Transaction History
+          </button>
+        </div>
+
+        <div className="flex justify-end space-x-16">
+          {transactions.map((transaction, index) => (
+            <div className="flex items-center space-x-4">
+              <>
+                {transaction.icon}
+                <span className="flex flex-col">
+                  <p className="text-[#2b4146] text-sm">{transaction.name}</p>
+                  <p className="text-[#001B21] text-base">
+                    {transaction.value}
+                  </p>
+                </span>
+              </>
+            </div>
+          ))}
+        </div>
+
+        <LineChart />
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
